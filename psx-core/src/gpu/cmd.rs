6d9c1082a3cbc5100972c3872ccdfe638cb9662a
Layle <ioncodes@users.noreply.github.com>
2025-10-04 02:07:19 +0200

    gp0 state

--- psx-core/src/gpu/cmd.rs
+++ psx-core/src/gpu/cmd.rs
@@ -1,21 +1,14 @@
+use proc_bitfield::bitfield;
+
 pub enum Gp0Command {
     Misc,
     PolygonPrimitive,
     LinePrimitive,
-    RectanglePrimitive,
+    RectanglePrimitive(DrawRectangleCommand),
     VramToVramBlit,
     CpuToVramBlit,
     VramToCpuBlit,
     Environment,
-}
-
-impl Gp0Command {
-    pub fn extra_data(&self) -> usize {
-        match self {
-            Gp0Command::RectanglePrimitive => 3,
-            _ => 0,
-        }
-    }
 }
 
 impl From<u32> for Gp0Command {
@@ -24,7 +17,7 @@ impl From<u32> for Gp0Command {
             0b000 => Gp0Command::Misc,
             0b001 => Gp0Command::PolygonPrimitive,
             0b010 => Gp0Command::LinePrimitive,
-            0b011 => Gp0Command::RectanglePrimitive,
+            0b011 => Gp0Command::RectanglePrimitive(DrawRectangleCommand(value)),
             0b100 => Gp0Command::VramToVramBlit,
             0b101 => Gp0Command::CpuToVramBlit,
             0b110 => Gp0Command::VramToCpuBlit,
@@ -40,12 +33,48 @@ impl std::fmt::Display for Gp0Command {
             Gp0Command::Misc => "Misc. Commands",
             Gp0Command::PolygonPrimitive => "Polygon Primitive",
             Gp0Command::LinePrimitive => "Line Primitive",
-            Gp0Command::RectanglePrimitive => "Rectangle Primitive",
+            Gp0Command::RectanglePrimitive(_) => "Rectangle Primitive",
             Gp0Command::VramToVramBlit => "VRAM to VRAM Blit",
             Gp0Command::CpuToVramBlit => "CPU to VRAM Blit",
             Gp0Command::VramToCpuBlit => "VRAM to CPU Blit",
             Gp0Command::Environment => "Environment Commands",
         };
         write!(f, "{}", name)
+    }
+}
+
+impl Gp0Command {
+    pub fn base_extra_data_count(&self) -> usize {
+        match self {
+            Gp0Command::RectanglePrimitive(cmd) => {
+                let mut base = 3;
+
+                if cmd.textured() {
+                    base += 1;
+                }
+
+                if cmd.size() == 0 {
+                    base += 1;
+                }
+
+                base
+            }
+            Gp0Command::VramToVramBlit => 3,
+            Gp0Command::CpuToVramBlit => 2,
+            Gp0Command::VramToCpuBlit => 2,
+            _ => 0,
+        }
+    }
+}
+
+bitfield! {
+    #[derive(Clone, Copy, PartialEq, Eq)]
+    pub struct DrawRectangleCommand(pub u32): Debug, FromStorage, IntoStorage, DerefStorage {
+        pub color: u32 @ 0..=23,
+        pub raw_texture: bool @ 24,
+        pub semi_transparent: bool @ 25,
+        pub textured: bool @ 26,
+        pub size: u32 @ 27..=28,
+        pub command: u32 @ 29..=31,
     }
 }

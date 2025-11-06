f26a505cec6d8a1178f6239d31d4c941ee01160a
Layle <ioncodes@users.noreply.github.com>
2025-11-06 15:06:21 +0100

    tty

--- psx-core/src/cpu/internal.rs
+++ psx-core/src/cpu/internal.rs
@@ -11,7 +11,6 @@ static TTY_LINE_BUFFER: OnceLock<Mutex<String>> = OnceLock::new();
 pub fn cpu_hooks() -> &'static HashMap<u32, HookHandler> {
     HOOKS.get_or_init(|| {
         let mut hooks = HashMap::new();
-        hooks.insert(0xBFC0D460, bios_putchar as HookHandler); // TODO: just hook the BIOS putchar function? would make it compatible with other BIOS
         hooks.insert(0x000000A0, bios_function_call_a as HookHandler);
         hooks.insert(0x800000A0, bios_function_call_a as HookHandler);
         hooks.insert(0x000000B0, bios_function_call_b as HookHandler);
@@ -30,6 +29,26 @@ pub fn tty_line_buffer() -> &'static Mutex<String> {
     TTY_LINE_BUFFER.get_or_init(|| Mutex::new(String::new()))
 }
 
+fn bios_function_call_a(cpu: &mut Cpu) {
+    if cpu.read_register(9) == 0x3C {
+        bios_putchar(cpu);
+    }
+
+    tracing::debug!(target: "psx_core::bios", function = %format!("A({:08X})", cpu.read_register(9)), "BIOS function called");
+}
+
+fn bios_function_call_b(cpu: &mut Cpu) {
+    if cpu.read_register(9) == 0x3D {
+        bios_putchar(cpu);
+    }
+
+    tracing::debug!(target: "psx_core::bios", function = %format!("B({:08X})", cpu.read_register(9)), "BIOS function called");
+}
+
+fn bios_function_call_c(cpu: &mut Cpu) {
+    tracing::debug!(target: "psx_core::bios", function = %format!("C({:08X})", cpu.read_register(9)), "BIOS function called");
+}
+
 fn bios_putchar(cpu: &mut Cpu) {
     let value = cpu.read_register(crate::regidx("$a0")) as u8 as char;
 
@@ -42,16 +61,4 @@ fn bios_putchar(cpu: &mut Cpu) {
     } else {
         tty_line_buffer().lock().unwrap().push(value);
     }
-}
-
-fn bios_function_call_a(cpu: &mut Cpu) {
-    tracing::debug!(target: "psx_core::bios", function = %format!("A({:08X})", cpu.read_register(9)), "BIOS function called");
-}
-
-fn bios_function_call_b(cpu: &mut Cpu) {
-    tracing::debug!(target: "psx_core::bios", function = %format!("B({:08X})", cpu.read_register(9)), "BIOS function called");
-}
-
-fn bios_function_call_c(cpu: &mut Cpu) {
-    tracing::debug!(target: "psx_core::bios", function = %format!("C({:08X})", cpu.read_register(9)), "BIOS function called");
 }
